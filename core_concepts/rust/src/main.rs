32df01b9e38c6f6d0f2294c8b808dd788f524203
ibg101 <ssksg101@gmail.com>
2025-03-02 01:24:19 +0100

    explain concurrency && parallelism

--- core_concepts/rust/src/main.rs
+++ core_concepts/rust/src/main.rs
@@ -1,5 +1,14 @@
-mod ownership;
+use simple_logger::SimpleLogger;
 
-fn main() {
-    
+mod ownership;
+mod concurrency;
+
+// by default, it's set to the "multi_thread" runtime && default worker threads == available CPU cores,
+// but can be modified and explicitly set to #[tokio::main(flavor = "single_thread")]
+// or if you want to constraint worker threads amount, then #[tokio::main(flavor = "multi_thread", worker_threads = X)]
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new().init().unwrap();
+
+    let _ = concurrency::basics().await;
 }
